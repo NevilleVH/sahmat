6bbfa2300aed938980e1a60bf3d57fd862d539a5
NevilleVH <1nevillevh@gmail.com>
2026-06-25 16:03:20 +0200

    Add title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.png';
-
+	import { page } from '$app/state'; 
+	
 	let { children } = $props();
+
+   let title = $derived.by(() => {
+	
+    const path = page.url.pathname.split("/").filter(Boolean);
+	path.unshift("Sahmat")
+    return path
+      .map(segment => segment.charAt(0).toUpperCase() + segment.slice(1))
+      .join(' - ');
+  });
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>{title}</title>
 </svelte:head>
 
 
@@ -14,8 +25,10 @@
 <style >
   :global(html), :global(body) {
 	background-color: aliceblue;
-    /* min-height: 100vh; */
+	/* height: 100%;
+	margin: 0;
+	overflow: hidden; */
   }
 
-  
+
 </style>
